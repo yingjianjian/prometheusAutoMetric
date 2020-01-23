bbb92b92750d855adebdc465b831424d5357a23b
yingjianjian <yingjj@isyscore.com>
2020-01-23 12:27:48 +0800

    init

---
+++ autoMetric/util/esApi.py
@@ -0,0 +1,31 @@
+from elasticsearch import Elasticsearch
+
+# print(es.nodes.stats())
+class esApi():
+    def __init__(self):
+        self.es = Elasticsearch([{'host': '10.30.30.25', 'port': 9200}], timeout=3600)
+        self.result = dict()
+        self.Value = dict()
+        self.esStatus = dict()
+    def es_status(self):
+       for key,value in self.es.nodes.stats()['nodes'].items():
+           self.Value['es_heap_used_percent'] = value['jvm']['mem']['heap_used_percent']           #75的时候进行GC   节点总是大约75%，那你节点正在承受内存方面的压力，这是一个告警，预示着你不久就会出现慢GC heap使用率一直在85%
+           self.Value['es_heap_used_in_bytes'] = value['jvm']['mem']['heap_used_in_bytes']
+           self.Value['es_young_collection_count'] = value['jvm']['gc']['collectors']['young']['collection_count']
+           self.Value['es_young_collection_millis'] = value['jvm']['gc']['collectors']['young']['collection_time_in_millis']
+           self.Value['es_old_collection_count'] = value['jvm']['gc']['collectors']['old']['collection_count']
+           self.Value['es_old_collection_millis'] = value['jvm']['gc']['collectors']['old']['collection_time_in_millis']
+           self.Value['es_index_total'] = value['indices']['indexing']['index_total']
+           self.Value['index_time_in_millis'] = value['indices']['indexing']['index_time_in_millis']
+           self.result[key] = self.Value
+       return self.result
+    def cluster_status(self):
+        esStatus = self.es.cluster.health()
+        self.esStatus['es_status'] = esStatus['status']
+        self.esStatus['es_unassigned_shards'] = esStatus['unassigned_shards']
+        return self.esStatus
+
+#
+# while True:
+#     a = esApi()
+#     a.es_status()
---
+++ autoMetric/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+import prometheus_client
+from prometheus_client import Counter,Gauge
+from prometheus_client.core import CollectorRegistry
+from django.views.generic import View
+from django.http import HttpResponse
+import random
+from .util import esApi
+
+# requests_total=Counter("request_count", "Total request count of the host") # 数值只增
+# class requests_count(View):
+#     def get(self,request):
+#         requests_total.inc(1)
+#         return HttpResponse(prometheus_client.generate_latest(requests_total),content_type="text/plain")
+
+REGISTRY = CollectorRegistry(auto_describe=False)
+
+# manageStatus = Gauge("manage_api_21","Api response stats is:",registry=REGISTRY)
+# random_value = Gauge("random_value","Random value of the request",registry=REGISTRY)
+# c = Counter("request_total","HTTP requests total",["method","clientip"],registry=REGISTRY)
+# g = Gauge('my_inprogress_requests','Description of gauge',['mylabelname'],registry=REGISTRY)
+
+
+
+class ApiResponse(View):
+    def get(self,request):
+        es_obj = esApi.esApi()
+        es_result = es_obj.es_status()
+        for node, values in es_result.items():
+            for key,value in values.items():
+                esStatus = Gauge("%s"%(key), "%s status is:" % (node), ["node","class"],
+                                 registry=REGISTRY)  # 数值可大可小
+                esStatus.labels(node,key).set(value)
+        # c.labels('get', '127.0.0.1').inc()
+        # c.labels('post', '127.0.0.1').inc(3)
+        # c.labels(method='get', clientip="192.168.0.1").inc()
+        # g.labels(mylabelname='str').set(3.6)
+        return HttpResponse(prometheus_client.generate_latest(REGISTRY),content_type="text/plain")
+
+# Create your views here.

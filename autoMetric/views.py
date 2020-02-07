d04dc3bfd72a5cdd224472500e31db470f5aa7f2
yingjianjian <yingjj@isyscore.com>
2020-02-07 15:03:45 +0800

    init

--- autoMetric/views.py
+++ autoMetric/views.py
@@ -13,7 +13,7 @@ esStatus = Gauge("elasticsearch", "elasticsearch status is:", ["node", "class"],
 
 
 REGISTRY_URL = CollectorRegistry(auto_describe=False)
-urlCollector = Gauge("watcherStatus","watcher Url status",["status","code"],registry=REGISTRY_URL)
+urlCollector = Gauge("watcherStatus","watcher Url status",["status","code","url"],registry=REGISTRY_URL)
 class ApiResponse(View):
     def get(self,request):
         es_obj = esApi.esApi()
@@ -25,12 +25,13 @@ class ApiResponse(View):
 
 class urlStatus(View):
     def get(self,request):
-        code = urlRequest.isciasStatus('http://111.0.120.180:38080/iscias/project/page/2019ncov')
+        urlObj = 'http://111.0.120.180:38080/iscias/project/page/2019ncov'
+        code = urlRequest.isciasStatus(urlObj)
         if code >= 400 and code <=599:
             status = "failed"
-            urlCollector.labels(status, code).set(0)
+            urlCollector.labels(status, code,urlObj).set(0)
         else:
             status = "success"
-            urlCollector.labels(status,code).set(1)
+            urlCollector.labels(status,code,urlObj).set(0)
         return HttpResponse(prometheus_client.generate_latest(REGISTRY_URL),content_type="text/plain")
 # Create your views here.
